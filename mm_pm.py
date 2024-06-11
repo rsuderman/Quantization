2cd9ead2e9f7c67306cbae2fd340f6f136f43066
Rob Suderman <rob.suderman@gmail.com>
2024-06-11 11:13:53 -0700

    Fix quantization for including offsets

--- mm_pm.py
+++ mm_pm.py
@@ -19,49 +19,62 @@ A = generate_input(A_SHAPE)
 B = generate_input(B_SHAPE)
 
 
-A = A + A_OFFSET
-B = B + B_OFFSET
+A = A + 1.0
+B = B + 1.0
 
 A = A * A_QUANT
 B = B * B_QUANT
 
-def quant_i8_per_tensor(A):
-    MIN = torch.min(A)
+def quant_i8_per_tensor(A, offset=True):
+    MIN = 0.0
     MAX = torch.max(A)
 
-    MIN = 0.0
+    if offset:
+        MIN = torch.min(A)
+
     RANGE = MAX - MIN
     OFFSET = MIN
     SCALE = 255.0 / RANGE
 
-    OFFSET = torch.full((A.shape[0],), OFFSET)
     SCALE = torch.full((A.shape[0],), SCALE)
-    A = (A - OFFSET.unsqueeze(1)) * SCALE.unsqueeze(1)
+    OFFSET = torch.full((A.shape[0],), OFFSET)
+    OFFSET = OFFSET * SCALE
+
+    A = A * SCALE.unsqueeze(1) - OFFSET.unsqueeze(1)
     A = torch.round(A)
     A = torch.clamp(A, 0, 255.0)
     A = A.to(torch.uint8)
-    return A, SCALE
+
+    SCALE = 1.0 / SCALE
+    return A, SCALE, -OFFSET
 
 
-def quant_i8_per_channel(A,axis=1):
-    MIN = 0
+def quant_i8_per_channel(A,axis=1, offset=False):
+    MIN = torch.full((A.shape[0],), 0.0)
     MAX = torch.max(A, axis=axis).values
+
+    if offset:
+        MIN = torch.min(A, axis=axis).values
+
     SCALE = 255.0 / (MAX - MIN)
-    A = A * SCALE.unsqueeze(axis)
+    OFFSET = MIN * SCALE
+    A = A * SCALE.unsqueeze(axis) - OFFSET.unsqueeze(axis)
+    SCALE = 1.0 / SCALE
+
     A = torch.round(A)
     A = torch.clamp(A, 0, 255.0)
     A = A.to(torch.uint8)
 
-    return A, SCALE
+    return A, SCALE, -OFFSET
 
 
-def mmt_quant(A, B, per_tensor=True):
+def mmt_quant(A, B, per_tensor=False, offset=True):
     if per_tensor:
-        A, A_SCALE = quant_i8_per_tensor(A)
-        B, B_SCALE = quant_i8_per_tensor(B)
+        A, A_SCALE, A_OFFSET = quant_i8_per_tensor(A, offset=offset)
+        B, B_SCALE, B_OFFSET = quant_i8_per_tensor(B, offset=offset)
     else:
-        A, A_SCALE = quant_i8_per_channel(A)
-        B, B_SCALE = quant_i8_per_channel(B)
+        A, A_SCALE, A_OFFSET = quant_i8_per_channel(A, offset=offset)
+        B, B_SCALE, B_OFFSET = quant_i8_per_channel(B, offset=offset)
 
     B = torch.transpose(B, 0, 1)
 
@@ -69,8 +82,17 @@ def mmt_quant(A, B, per_tensor=True):
     B = B.to(torch.float)
     MM = torch.mm(A, B)
 
-    MM = MM / A_SCALE.unsqueeze(1)
-    MM = MM / B_SCALE.unsqueeze(0)
+    A_FIX = torch.sum(B, axis=0, keepdim=True) * A_OFFSET.unsqueeze(1)
+    B_FIX = torch.sum(A, axis=1, keepdim=True) * B_OFFSET.unsqueeze(0)
+    AB_FIX = A_OFFSET.unsqueeze(1) * B_OFFSET.unsqueeze(0) * A.shape[1]
+
+    MM = MM - A_FIX - B_FIX
+    MM = MM + AB_FIX
+
+    # Apply the scaling once normalization is fixed:
+    MM = MM * A_SCALE.unsqueeze(1)
+    MM = MM * B_SCALE.unsqueeze(0)
+
     return MM
 
 
